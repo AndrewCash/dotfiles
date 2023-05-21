572dac036711fda1066c71bb8fe961dbe12cd2e1
Andrew Cash <andrew@csh.casa>
2023-05-20 21:31:50 -0500

    Add color scheme

--- configs/nvim/lua/andrewcash/packer.lua
+++ configs/nvim/lua/andrewcash/packer.lua
@@ -54,6 +54,14 @@ return require('packer').startup(function(use)
     }
   }
 
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
