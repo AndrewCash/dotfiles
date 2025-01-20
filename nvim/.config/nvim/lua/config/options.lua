7aec7feaa6921d7887ede40983d130835338524c
Andrew Cash <andrew@csh.casa>
2025-01-20 05:58:42 -0600

    copy options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,45 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+-- netrw settings
+vim.g.netrw_banner = 0 -- Hide banner
+--vim.g.netrw_browse_split = 4 -- Open in previous window
+vim.g.netrw_altv = 1 -- Open with right splitting
+vim.g.netrw_liststyle = 3 -- Tree-style view
+
+vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>v', ':Vexplore<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>t', ':Texplore<CR>', { noremap = true })
+
