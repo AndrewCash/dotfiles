4bb367942c13a0b19862afd5a2ed33a3622bdd9b
Andrew Cash <andrew@csh.casa>
2025-02-23 08:33:53 -0600

    emacs stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,17 +1,2 @@
 (setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
- '(package-selected-packages
-   '(web-mode glsl-mode typescript-mode tree-sitter-langs lsp-ivy lsp-ui lsp-mode flycheck zig-mode which-key visual-fill-column theme-buffet solarized-theme rainbow-delimiters org-bullets olivetti nov markdown-mode magit ivy-rich helpful gptel go-mode doom-themes doom-modeline counsel command-log-mode auto-package-update all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
